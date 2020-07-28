681dff8ae647c831ac5d85602533a04dfb8cc4db
Jhonny Chamoun <jhonny.chamoun@gmail.com>
2020-07-28 20:43:29 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,9 +1,14 @@
 # Code your solution here!
 def run_guessing_game
-  hidden_number = rand(5)+1
+  hidden_number = rand(6)+1
   prompt_user
   guessed_number = get_user_input
 end
 
 def prompt_user
-  puts ""
+  puts "Guess the number."
+end
+
+def get_user_input
+  gets.
+end
